b060941122a9439f28f1d3e79b9a15bf02b02d20
João Vítor de Luiz Alves <joaovitordeluizalves@gmail.com>
2022-06-21 12:45:11 -0300

    CTF update: Bugfix

--- Python/Capture_the_flag/cmd/paths.py
+++ Python/Capture_the_flag/cmd/paths.py
@@ -1,38 +1,114 @@
 import os.path
 from pathlib import Path
 from configparser import ConfigParser
+import PySimpleGUI as sg
 
 FILE = '.tags.ini'
+HOME = str(Path.home())
+AUTOF = HOME + "/automatic_files/"
+
+class basefolders_window:
+    def __init__(self):
+        layout = [
+            [sg.Push(), sg.Text("NAME YOUR FIRSTS FOLDERS", font="Arial 16 bold", text_color="#000000"), sg.Push()],
+            [sg.VPush()],
+            [sg.Text("Define Tag and Folder: "), sg.Input(size=(5, 1), key="-TAG-", default_text="TAG", tooltip="Three Uppercase letters only"), sg.Input(size=(14,1), key="-FOLDER-", default_text="Folder", tooltip="Your folder name"), sg.Button("ADD", key="-ADD-")],
+            [sg.VPush()],
+            [sg.Text("TAGS AND FOLDERS")],
+            [sg.Text("Nothing yet", key="-TAGSNFOLDERS-")],
+            [sg.VPush()],
+            [sg.Push(), sg.Button("DELETE", key="-DELLAST-", tooltip="Delete last folder path and tag", button_color="#9e2a2b"),sg.Button("SUBMIT", key="-SUBMIT-", button_color="#3a5a40"), sg.Push()]
+        ]
+
+        self.window = sg.Window("First Folders").layout(layout)
+    def init(self):
+        TAGS = []
+        FOLDERS = []
+        while True:
+            event, value = self.window.read(timeout=100)
+            if len(TAGS) != 0 and len(FOLDERS) != 0:
+                TnFlist = ""
+                for tag, folder in zip(TAGS, FOLDERS):
+                    TnFlist += f"{tag}  {folder}\n"
+
+                self.window["-TAGSNFOLDERS-"].update(TnFlist)
+
+            if event == sg.WIN_CLOSED:
+                exit()
+            elif event == "-ADD-":
+                if value is None:
+                    raise Exception("Unknown Error")
+
+                #TAG format verification
+                if not value["-TAG-"].isalpha() or len(value["-TAG-"]) != 3:
+                    sg.popup_error(f"TAGS MUST BE THREE UPPERCASE LETTERS ONLY\n LENGTH: {len(value['-TAG-'])}")
+                    continue
+
+                tag = value["-TAG-"].upper()
+                folder = value["-FOLDER-"]
+
+                TAGS.append(tag)
+                FOLDERS.append(AUTOF+folder+"/")
+                continue
+
+            elif event == "-DELLAST-":
+                if len(TAGS) == 0 and len(FOLDERS) == 0:
+                    sg.popup_error("Can not delete nothing.")
+                    continue
+
+                TAGS.pop(-1)
+                FOLDERS.pop(-1)
+                continue
+
+            elif event == "-SUBMIT-":
+                self.window.close()
+                return (TAGS, FOLDERS)
+
+
 
 
 def Config():
+    if not os.path.exists(AUTOF):
+        os.mkdir(AUTOF)
+
     if not os.path.exists(FILE):
-        open(FILE, "x")
+        init()
 
     config = ConfigParser()
     config.read(FILE)
     return config
 
-HOME = str(Path.home())
-AUTOF = HOME + "/automatic_files/"
 
-if __name__ != "__main__":
+
+#create and populate the config file for the first time
+def init():
+    #set the firsts tags and folders
+    basefolder = basefolders_window()
+    TAGS, FOLDERS = basefolder.init()
+
+    #create .ini file to populated
+    with open(FILE, "x"):
+        pass
+
+    #make the file a config
+    config = ConfigParser()
+    config.read(FILE)
+
+    config.add_section("TAGS")
+
+    for tag, folder in zip(TAGS, FOLDERS):
+        print(tag, folder)
+        config.set("TAGS", tag, folder)
+
+    with open(FILE, "w") as ConfigFile:
+        config.write(ConfigFile)
+
+
+def start():
     config = Config()
 
-    if len(config.sections()) == 0:
-        config.add_section("tags")
-        config.add_section("autofilter")
-
-    if len(config["tags"]) == 0:
-        config.set("tags", "ICO", AUTOF + "icons/")
-        config.set("tags", "ART", AUTOF + "arts/")
-        config.set("tags", "PDF", AUTOF + "pdf/")
-        config.set("tags", "PHT", AUTOF + "photos/")
-        with open(FILE, "w") as ConfigFile:
-            config.write(ConfigFile)
-
     tags_and_folders = {}
-    TAGS = config["tags"]
+    TAGS = config["TAGS"]
     for elem in TAGS:
         tags_and_folders[elem.upper()] = TAGS[elem]
 
@@ -41,3 +117,40 @@ if __name__ != "__main__":
     for path in tags_and_folders.values():
         if not os.path.exists(path):
             os.mkdir(path)
+    return tags_and_folders
+
+def ExistsOnConfigfile(KeyVal:str, information:str, return_element:bool=False, return_alterElement:bool=False):
+
+    config = Config()
+    valid = {"key", "value"}
+
+
+    if KeyVal not in valid:
+        raise ValueError("results: KeyVal must be one of %r." % valid)
+
+
+    if KeyVal == "key":
+        for key, value in config["TAGS"].items():
+            if key == information:
+                if return_element:
+                    return True, key
+                elif return_alterElement:
+                    return True, value
+                else:
+                    return True, None
+
+        return False, None
+
+    elif  KeyVal == "value":
+        for key, value in config["TAGS"].items():
+            if value == information:
+                if return_element:
+                    return True, value
+                elif return_alterElement:
+                    return True, key
+                else:
+                    return True, None
+
+        return False, None
+
+
